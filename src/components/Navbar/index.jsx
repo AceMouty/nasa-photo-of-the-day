e80e1bab76095fb3a0ef6c96615ea64692188b20
kyle Guerrero <kyleguerrero85@gmail.com>
2020-06-02 19:57:43 -0500

    styling: completed basic layout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Navbar(){
+  
+  // date to be held from the form
+  const [userDate, setUserDate] = useState();
+
+  // set max date on initial render
+  useEffect(() => {
+    const datePicker = document.querySelector("input[type=date]")
+    datePicker.max = new Date().toISOString().split("T")[0];
+  })
+
+  function handleChange(e) {
+    setUserDate(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(userDate);
+    
+  }
+
   return (
     <>
       <div className="logo-container">
         <h1>NASA: Daily Feature</h1>
       </div>
-      <div className="relative bg-white px-2 rounded-md">
-        <input type="text" placeholder="Search..." className="w-40 focus:outline-none"/>
-        <button>
+      <form onSubmit={handleSubmit} className="relative bg-white px-2 rounded-md">
+        <input type="date" required onChange={handleChange} className="w-40 focus:outline-none"/>
+        <button type="submit">
           <i className="fa fa-search left-auto"/>
         </button>
-      </div>
+      </form>
     </>
   )
 }
