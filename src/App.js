99e31e966a56bfdde884152376f441aae0936399
kyle Guerrero <kyleguerrero85@gmail.com>
2020-06-03 20:07:07 -0500

    chore: move all Nav jsx to component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Card from './components/Card'
 function App() {
   return (
     <div className="App flex flex-col min-h-screen">
-      <header className="bg-blue-600 flex justify-center items-center flex-col md:justify-between md:items-center md:flex-row py-3 px-6">
-        <Navbar/>
-      </header>
+      <Navbar/>
       <main className="flex justify-center items-center flex-1">
         <Card/>      
       </main>
